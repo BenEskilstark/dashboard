9c344f2e681409c386a5664178ab5e013934456d
Benjamin Eskildsen <eskildsen.ben@gmail.com>
2022-08-28 09:02:02 -0400

    add table UI

--- js/index.js
+++ js/index.js
@@ -1,19 +1,63 @@
 const axios = require('axios');
+const {Table} = require('bens_ui_components');
 const React = require('react');
+const ReactDOM = require('react-dom');
+const {useState, useEffect, useMemo} = React;
 
 console.log("connecting");
 
 const axiosInstance = axios.create({
   baseURL: 'https://ant-analytics.herokuapp.com',
 });
-
 // for localhost:
 // const axiosInstance = axios;
 
-axiosInstance
-  .get('/dashboard', {params: {hostname: 'www.antocracy.io'}})
-  // .get('/dashboard', {hostname: 'www.antocracy.io'})
-  .then(res => {
-    console.log(res.data);
-  });
-;
+const HOSTNAMES = [
+  '*',
+  'www.antocracy.io',
+  'sidewalk-empire.herokuapp.com',
+  'electron',
+  'localhost',
+];
+
+
+function Main(props) {
+  const [rows, setRows] = useState([]);
+  const [hostname, setHostname] = useState('*');
+  const [hostnameOptions, setHostnameOptions] = useState([HOSTNAMES]);
+
+  // getting data
+  useEffect(() => {
+    axiosInstance
+      .get('/dashboard', {params: {hostname}})
+      .then(res => {
+        console.log(res.data);
+        setRows(res.data);
+      });
+  }, [hostname]);
+
+  return (
+    <div>
+      <Table
+        columns={{
+          hostname: {displayName: 'Hostname'},
+          path: {displayName: 'Path'},
+          map: {displayName: 'Map'},
+          num_visits: {displayName: 'Visits'},
+          num_unique_visits: {displayName: 'Unique Visits'},
+          last_visited: {displayName: 'Last Visited'},
+        }}
+        rows={rows}
+      />
+    </div>
+  );
+}
+
+
+function renderUI(): void {
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('container'),
+  );
+}
+renderUI();
